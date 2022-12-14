8073dba2d8b32e65b24ec65be2e8539b54fe4cd2
Atinuke Ayangade <aayangade@gmail.com>
2022-12-14 17:42:08 -0500

    in API, convert JSON dictionary data to an array of restaurants, update view controller's restaurant array

--- yelpy_starter_2/Yelpy/Network/API.swift
+++ yelpy_starter_2/Yelpy/Network/API.swift
@@ -11,7 +11,8 @@ import Foundation
 
 struct API {
     
-    static func getRestaurants(completion: @escaping ([[String:Any]]?) -> Void) {
+    //update escape method from [[String: Any]]? to [Restaurant]?
+    static func getRestaurants(completion: @escaping ([Restaurant]?) -> Void) {
         
         // ––––– TODO: Add your own API key!
         let apikey = ""
@@ -43,7 +44,17 @@ struct API {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 // 2. Grab the businesses data and convert it to an array of dictionaries
                 //    for each restaurant
-                let restaurants = dataDictionary["businesses"] as! [[String: Any]]
+                let restDictionaries = dataDictionary["businesses"] as! [[String: Any]]
+                
+                //variable to store array of Restaurants
+                var restaurants: [Restaurant] = []
+                
+                //use each restaurant dictionary to initialize Restaurant object
+                for dictionary in restDictionaries{
+                    let restaurant = Restaurant.init(dict:dictionary)
+                    restaurants.append(restaurant)//add to restaurants array
+                }
+                
                 // 3. completion is an escaping method  which allows the data to be used
                 //    outside of the closure
                 return completion(restaurants)
